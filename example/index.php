5da8a5160bdaf46543fad36ed866a4ad2effd3c4
Roman Ožana <roman@ozana.cz>
2021-11-14 10:14:36 +0100

    Upgrade Bootstrap CDN link

--- example/index.php
+++ example/index.php
@@ -7,7 +7,7 @@
 
 	<title>Ical Parser example</title>
 
-	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
 <body>
