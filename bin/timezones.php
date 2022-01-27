d8de406746fc97b6e7a048b0606479f20f558e73
Roman Ožana <roman@ozana.cz>
2022-01-27 10:48:25 +0100

    Move timezones to bin

--- bin/timezones.php
+++ bin/timezones.php
@@ -11,10 +11,10 @@ $windowstimezonexml = new DOMDocument();
 $windowstimezonexml->load('https://raw.githubusercontent.com/unicode-org/cldr/master/common/supplemental/windowsZones.xml');
 $zones = $windowstimezonexml->getElementsByTagName('mapZone');
 foreach ($zones as $zone) {
-	if($zone->getAttribute('territory') === '001') {
+	if ($zone->getAttribute('territory') === '001') {
 		$windows_timezones[$zone->getAttribute('other')] = $zone->getAttribute('type');
 	}
 }
 
-file_put_contents(__DIR__ . '/windows_timezones.php', "<?php\n\$windows_timezones = " . var_export($windows_timezones, true) . ';');
+file_put_contents(__DIR__ . '/../src/WindowsTimezones.php', "<?php\n\$windows_timezones = " . var_export($windows_timezones, true) . ';');
 
